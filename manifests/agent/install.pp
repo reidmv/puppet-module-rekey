37508e0fdf5f604246cf196e4ad279fd1dc51988
Reid Vandewiele <reid@puppetlabs.com>
2014-04-13 13:18:12 -0700

    Make agent install dependent on signed cert
    Assume the signed cert will be retrieved from the master prior to
    installing the new key. The reason for this requirement is that if a
    cert doesn't exist, an agent will automatically request one from the
    master. If the master gives it a cert signed by the old CA, the agent is
    effectively disabled and cannot resume until manual intervention removes
    the conflicting key and/or certificate. Therefore, don't install the key
    unless the certificate is also available and can be installed at the
    same time.

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -10,29 +10,33 @@ class rekey::agent::install {
   $clientcert = $rekey::agent::prep::clientcert
 
   File {
-    backup => false,
+    ensure  => file,
+    owner   => $::id,
+    require => File["${::puppet_ssldir}/certs/${clientcert}.pem"],
+    backup  => false,
+  }
+
+  # Ensure that a rekeyed certificate exists and is installed
+  file { "${::puppet_ssldir}/certs/${clientcert}.pem":
+    source  => "puppet:///modules/rekey/var/${clientcert}.pem",
+    mode    => '0644',
+    require => undef,
   }
 
   # Ensure that the newly generated keys are installed for use by the Puppet
   # agent and that the remenants of the old CA are cleared out
   file { "${::puppet_ssldir}/private_keys/${clientcert}.pem":
-    ensure  => file,
     source  => $keyfile,
-    owner   => $::id,
     mode    => '0600',
     require => Exec['rekey_generate_new_private_key'],
   }
   file { "${::puppet_ssldir}/public_keys/${clientcert}.pem":
-    ensure  => file,
     source  => $pubfile,
-    owner   => $::id,
     mode    => '0644',
     require => Exec['rekey_generate_new_public_key'],
   }
   file { "${::puppet_ssldir}/certificate_requests/${clientcert}.pem":
-    ensure  => file,
     source  => $csrfile,
-    owner   => $::id,
     mode    => '0640',
     require => Exec['rekey_generate_new_csr'],
   }
@@ -43,8 +47,8 @@ class rekey::agent::install {
   # install the cert now we can't ensure the agent will be keyed to the new
   # one.
   file { "${::puppet_ssldir}/certs/ca.pem":
-    ensure  => file,
     source  => "${::puppet_vardir}/rekey_ca_crt.pem",
+    mode    => '0644',
     require => File["${::puppet_vardir}/rekey_ca_crt.pem"],
   }
 
